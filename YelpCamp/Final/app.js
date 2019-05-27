9b126a7a2c60b147df70531cc7aaf00cf33aaa54
Frank Runfola <frunfola1229@gmail.com>
2019-05-27 15:54:35 +0000

    commit new port

--- YelpCamp/Final/app.js
+++ YelpCamp/Final/app.js
@@ -53,6 +53,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3001, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
